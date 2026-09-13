70e2bfe6e2e7de091959e544e0672f4b8faa25bc
Dipanshu-verma252004 <dipanshuverma252004@gmail.com>
2026-09-13 00:37:41 -0700

    feat: connect complete portfolio admin modules

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,16 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'as' => 'admin.', 'pr
     Route::group(['middleware' => ['auth', 'admin']], function () {
         Route::get('/', 'DashboardController@index')->name('dashboard');
         Route::get('/dashboard', 'DashboardController@index');
+        Route::get('/about', 'AboutController@edit')->name('about.edit');
+        Route::put('/about', 'AboutController@update')->name('about.update');
         Route::resource('/projects', 'ProjectController')->except(['show']);
+        Route::resource('/skills', 'SkillCategoryController')->parameters(['skills' => 'skill'])->except(['show']);
+        Route::resource('/experience', 'ExperienceController')->except(['show']);
+        Route::resource('/services', 'ServiceController')->except(['show']);
+        Route::resource('/blog', 'BlogController')->except(['show']);
+        Route::get('/messages', 'ContactMessageController@index')->name('messages.index');
+        Route::get('/messages/{contactMessage}', 'ContactMessageController@show')->name('messages.show');
+        Route::patch('/messages/{contactMessage}/status', 'ContactMessageController@updateStatus')->name('messages.status');
+        Route::delete('/messages/{contactMessage}', 'ContactMessageController@destroy')->name('messages.destroy');
     });
 });
